6fa746958f93a8b19b34a5f91f1f7eac8911f8d6
github username <github email address>
2020-11-17 15:06:19 -0600

    MeowingLab

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,21 @@
-## code your solution here. 
+class Cat
+    def meow
+     puts "meow!"
+    end
+        attr_accessor :name
+        #attr_reader :name
+      
+    # def name=(cats_name)
+    #     @this_cats_name = cats_name
+    #   end
+    
+    #   def name
+    #     @this_cats_name
+    #   end
+end 
+
+maru = Cat.new
+maru.name = "Maru"
+maru.meow 
+
+    
